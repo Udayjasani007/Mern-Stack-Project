9a30d9ec35c0c79d502ef0f9440d1aef22aa8052
Udayjasani007 <111590778+Udayjasani007@users.noreply.github.com>
2022-12-23 00:12:39 +0530

    upload mern stack project
    upload mern stack project

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const dotenv = require("dotenv");
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");   
+
+dotenv.config({path:'./config.env'})
+
+//middleware
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then(() => {
+    console.log('connection successful');
+}).catch((err) => console.log('no collection'));
+
+
+
+const middleware = (req, res, next) => {
+    console.log("hello my middleare");
+    next();
+}
+
+
+app.get('/', (req, res)=> {
+    res.send('hello world form the server');     
+});
+app.get('/about',middleware, (req, res)=> {
+    res.send('hello about world form the server');     
+});
+app.get('/contact', (req, res)=> {
+    res.send('hello contact world form the server');     
+});
+app.get('/signin', (req, res)=> {
+    res.send('hello signin world form the server');     
+});
+app.get('/signup', (req, res)=> {
+    res.send('hello signup world form the server');     
+});
+
+app.listen(3000, ()=> {
+    console.log('server is running at 3000');
+})
+
+console.log("subscribe to my channel");
